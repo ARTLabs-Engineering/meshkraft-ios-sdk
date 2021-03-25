563f3e0284d3c198a87b4ca1c71b0af53a0c6464
irmakozonay <irmakoz@msn.com>
2021-03-25 11:43:43 +0300

    stat sessioneid

--- Meshkraft/Classes/Meshkraft.swift
+++ Meshkraft/Classes/Meshkraft.swift
@@ -76,7 +76,6 @@ public class Meshkraft : NSObject, QLPreviewControllerDataSource {
     public func getModelURL(productSKU: String, completion: @escaping (_ modelUrl: String?, _ errorMessage: String?) -> Void) {
         if let url = URL(string: "https://" + (Meshkraft.testing ? "staging." : "") + "api.artlabs.ai/product/" + productSKU) {
         var request = URLRequest(url: url)
-            print(url)
         request.setValue(Meshkraft.apiKey, forHTTPHeaderField: "x-api-key")
             let task = URLSession.shared.dataTask(with: request) { data, response, error in
                 var errorMessage: String? = nil
--- Meshkraft/Classes/MeshkraftStat.swift
+++ Meshkraft/Classes/MeshkraftStat.swift
@@ -20,6 +20,7 @@ struct MeshkraftEvent: Encodable {
 }
 
 class MeshkraftStat : NSObject {
+    static let sessionId = UUID().uuidString
     
     static func sdkInit(){
         sendStat(MeshkraftEvent(key: "SDK_INIT", segmentation: MeshkraftEvent.Segmentation(sku: nil)))
@@ -34,19 +35,12 @@ class MeshkraftStat : NSObject {
     }
     
     static func sendStat(_ event: MeshkraftEvent){
-        
-        guard let eventJsonData = try? JSONEncoder().encode([event]) else {
-            return
-        }
-        if let eventJson = String(data: eventJsonData, encoding: .utf8) {
-            let urlString = "https://countly.artlabs.ai?app_key=aa62f3a1ade0c9026201c819a6450d5e0edd84f3&device_id=x&events=" + eventJson
-            print(urlString)
+        guard let eventJsonData = try? JSONEncoder().encode([event]) else { return }
+        guard let eventJson = String(data: eventJsonData, encoding: .utf8) else { return }
+        if let encodedEventJson = eventJson.addingPercentEncoding(withAllowedCharacters: .urlHostAllowed) {
+            let urlString = "https://countly.artlabs.ai/i?app_key=aa62f3a1ade0c9026201c819a6450d5e0edd84f3&device_id=" + sessionId + "&events=" + encodedEventJson
             if let url = URL(string: urlString) {
-                let task = URLSession.shared.dataTask(with: url) {(data, response, error) in
-                    guard let data = data else { return }
-                    print(String(data: data, encoding: .utf8)!)
-                }
-                task.resume()
+                URLSession.shared.dataTask(with: url).resume()
             }
         }
     }
